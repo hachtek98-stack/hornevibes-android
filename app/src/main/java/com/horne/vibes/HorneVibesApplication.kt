f98ee98ad422e850ab8a0ff769a324bfe36d7f94
hachtek98-stack <hachtek98@gmail.com>
2026-02-23 19:03:34 +0300

    Create HorneVibesApplication class with Hilt annotation

--- app/src/main/java/com/horne/vibes/HorneVibesApplication.kt
+++ app/src/main/java/com/horne/vibes/HorneVibesApplication.kt
@@ -1,7 +1,6 @@
-package com.horne.vibes
-
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HorneVibesApplication : Application()
+class HorneVibesApplication : Application() {
+}
